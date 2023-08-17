69fc30a07bfe70e0137d31364a632a4ce82c6987
Sebi94nbg <sebastian.kraetzig@4g-server.eu>
2023-08-17 13:58:33 +0200

    Add PHPUnit test for `Template`

--- laravel/tests/Feature/TemplateTest.php
+++ laravel/tests/Feature/TemplateTest.php
@@ -2,9 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Models\Template;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TemplateTest extends TestCase
@@ -42,5 +42,36 @@ class TemplateTest extends TestCase
         $response = $this->actingAs($this->user)->get(route('templates'));
         $response->assertStatus(200);
         $response->assertViewIs('templates');
+    }
+
+    /**
+     * Test, that the user can access the "add template" page, when he is authenticated.
+     */
+    public function test_add_template_page_gets_displayed_when_authenticated(): void
+    {
+        $response = $this->actingAs($this->user)->get(route('template.add'));
+        $response->assertStatus(200);
+        $response->assertViewIs('template.add');
+    }
+
+    /**
+     * Test, that the user gets redirected to the templates overview, when the requested template ID for the edit page does not exist.
+     */
+    public function test_edit_template_page_gets_redirected_to_overview_when_template_id_does_not_exist(): void
+    {
+        $response = $this->actingAs($this->user)->get(route('template.edit', ['template_id' => 1337]));
+        $response->assertRedirect(route('templates'));
+    }
+
+    /**
+     * Test, that the user can access the "edit template" page, when the requested template ID for the edit page exists.
+     */
+    public function test_edit_template_page_gets_displayed_when_template_id_exists(): void
+    {
+        $template = Template::factory()->create();
+
+        $response = $this->actingAs($this->user)->get(route('template.edit', ['template_id' => $template->id]));
+        $response->assertViewIs('template.edit');
+        $response->assertViewHas('template');
     }
 }
