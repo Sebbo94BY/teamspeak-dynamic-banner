da285a48f172d0ef6e89143f4d4849a80d5117b4
Sebi94nbg <sebastian.kraetzig@4g-server.eu>
2023-08-17 12:44:05 +0200

    Add PHPUnit test for `Administration: Roles`

---
+++ laravel/tests/Feature/AdministrationRolesTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AdministrationRolesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected User $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        // Run the DatabaseSeeder
+        $this->seed();
+
+        $this->user = User::factory()->create();
+        $this->user->syncRoles('Roles Admin');
+    }
+
+    /**
+     * Test, that the user gets redirected to the login, when he is unauthenticated.
+     */
+    public function test_user_gets_redirected_to_login_when_unauthenticated(): void
+    {
+        $response = $this->get(route('administration.roles'));
+        $response->assertStatus(302);
+        $response->assertRedirect(route('login'));
+    }
+
+    /**
+     * Test, that the user can access the page, when he is authenticated.
+     */
+    public function test_page_gets_displayed_when_authenticated(): void
+    {
+        $response = $this->actingAs($this->user)->get(route('administration.roles'));
+        $response->assertStatus(200);
+        $response->assertViewIs('administration.roles');
+    }
+}
