71a7dda8806f1f8a6b2fc16086a2ddc06906fe8b
Sebi94nbg <sebastian.kraetzig@4g-server.eu>
2023-08-17 12:27:48 +0200

    Add PHPUnit test for `Authenticate` middleware

---
+++ laravel/tests/Feature/Middleware/AuthenticateTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature\Middleware;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AuthenticateTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Test, that the user gets redirected to the login if at least one user exists and is not authenticated.
+     */
+    public function test_redirect_to_login_if_unauthenticated(): void
+    {
+        User::factory()->create();
+
+        $response = $this->get(route('dashboard'));
+        $response->assertRedirect(route('login'));
+    }
+
+    /**
+     * Test, that the user gets redirected to the installer if no user exists yet.
+     */
+    public function test_redirect_to_installer_if_no_user_exists(): void
+    {
+        $response = $this->get(route('dashboard'));
+        $response->assertRedirect(route('setup.installer.requirements'));
+    }
+}
