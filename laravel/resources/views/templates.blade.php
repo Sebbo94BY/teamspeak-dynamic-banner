ab967960c15a16b206a7d321a02ef344d05528a4
Sebi94nbg <sebastian.kraetzig@4g-server.eu>
2023-08-20 15:56:39 +0200

    Fix never printed success and error messages

--- laravel/resources/views/templates.blade.php
+++ laravel/resources/views/templates.blade.php
@@ -14,9 +14,14 @@
                 </div>
 
                 <div class="card-body">
-                    @if (session('status'))
+                    @if (session('success'))
                         <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                            {{ session('message') }}
+                        </div>
+                    @endif
+                    @if (session('error'))
+                        <div class="alert alert-danger" role="alert">
+                            {{ session('message') }}
                         </div>
                     @endif
 
