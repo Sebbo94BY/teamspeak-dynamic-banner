1962b47dc8ad7f12318482ddce096bc569eb3a26
Sebi94nbg <sebastian.kraetzig@4g-server.eu>
2023-08-22 23:17:55 +0200

    Add Template model relationship test

--- laravel/tests/Unit/TemplateTest.php
+++ laravel/tests/Unit/TemplateTest.php
@@ -2,7 +2,11 @@
 
 namespace Tests\Unit;
 
+use App\Models\Banner;
+use App\Models\BannerTemplate;
+use App\Models\Instance;
 use App\Models\Template;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -21,7 +25,14 @@ class TemplateTest extends TestCase
     {
         $template = Template::factory()->create();
 
+        BannerTemplate::factory()->for(
+            Banner::factory()->for(
+                Instance::factory()->create()
+            )->create()
+        )->for($template)->create();
+
         $this->assertIsBool($template->isUsedByBannerId(1));
         $this->assertIsBool($template->isEnabledForBannerId(1));
+        $this->assertInstanceOf(Collection::class, $template->banner_templates());
     }
 }
