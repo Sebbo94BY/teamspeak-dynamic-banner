46df1b085dda44acf021693defc9e849539a19cb
Sebi94nbg <sebastian.kraetzig@4g-server.eu>
2023-09-03 14:13:26 +0200

    BannerConfigurationController: Simplify request validation code

--- laravel/app/Http/Requests/BannerConfigurationUpsertRequest.php
+++ laravel/app/Http/Requests/BannerConfigurationUpsertRequest.php
@@ -9,21 +9,36 @@ use Illuminate\Foundation\Http\FormRequest;
 class BannerConfigurationUpsertRequest extends FormRequest
 {
     /**
+     * The banner template, which should get upserted with this request.
+     */
+    protected BannerTemplate $banner_template;
+
+    /**
+     * Prepare the data for validation.
+     */
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'banner_template_id' => $this->route('banner_template_id'),
+        ]);
+
+        try {
+            $this->banner_template = BannerTemplate::findOrFail($this->banner_template_id);
+        } catch (ModelNotFoundException) {
+            // The below rule for `banner_template_id` will fail and handle this exception.
+            // However, the other rules need a defined variable with the respective accessible properties,
+            // so we simply return the first banner template.
+            $this->banner_template = BannerTemplate::first();
+        }
+    }
+
+    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, mixed>
      */
     public function rules()
     {
-        try {
-            $banner_template = BannerTemplate::findOrFail($this->banner_template_id);
-        } catch (ModelNotFoundException) {
-            // The below rule for `banner_template_id` will fail and handle this exception.
-            // However, the other rules need a defined variable with the respective accessible properties,
-            // so we simply return the first banner template.
-            $banner_template = BannerTemplate::first();
-        }
-
         return [
             'banner_template_id' => ['required', 'integer', 'exists:App\Models\BannerTemplate,id'],
             'redirect_url' => ['nullable', 'url'],
@@ -34,10 +49,10 @@ class BannerConfigurationUpsertRequest extends FormRequest
             'configuration.banner_configuration_id.*' => ['integer', 'exists:App\Models\BannerConfiguration,id'],
 
             'configuration.x_coordinate' => ['required', 'array', 'min:1'],
-            'configuration.x_coordinate.*' => ['integer', 'min:0', 'max:'.$banner_template->template->width],
+            'configuration.x_coordinate.*' => ['integer', 'min:0', 'max:'.$this->banner_template->template->width],
 
             'configuration.y_coordinate' => ['required', 'array', 'min:1'],
-            'configuration.y_coordinate.*' => ['integer', 'min:0', 'max:'.$banner_template->template->height],
+            'configuration.y_coordinate.*' => ['integer', 'min:0', 'max:'.$this->banner_template->template->height],
 
             'configuration.text' => ['required', 'array', 'min:1'],
             'configuration.text.*' => ['string', 'max:255'],
@@ -46,7 +61,7 @@ class BannerConfigurationUpsertRequest extends FormRequest
             'configuration.fontfile_path.*' => ['string'],
 
             'configuration.font_size' => ['required', 'array', 'min:1'],
-            'configuration.font_size.*' => ['integer', 'min:1', 'max:'.$banner_template->template->height],
+            'configuration.font_size.*' => ['integer', 'min:1', 'max:'.$this->banner_template->template->height],
 
             'configuration.font_angle' => ['required', 'array', 'min:1'],
             'configuration.font_angle.*' => ['integer', 'min:0', 'max:360'],
