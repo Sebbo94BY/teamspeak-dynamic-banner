1ac1909e5252c46fac1efc34d426e283c6c10e93
MajorOli <63414715+MajorOli@users.noreply.github.com>
2023-09-09 15:16:25 +0200

    https://github.com/Sebbo94BY/teamspeak-dynamic-banner/pull/139#discussion_r1320410793

--- laravel/resources/views/inc/standard-alerts.blade.php
+++ laravel/resources/views/inc/standard-alerts.blade.php
@@ -8,16 +8,24 @@
     </div>
 @endif
 @if (session('error'))
-    <div class="alert alert-danger" role="alert">
-        {{ session('message') }}
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="alert alert-danger" role="alert">
+                {{ session('message') }}
+            </div>
+        </div>
     </div>
 @endif
 @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
     </div>
 @endif
