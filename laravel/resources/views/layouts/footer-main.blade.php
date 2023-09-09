a410e20468b0620aaada93b8c5926a880f0cc462
MajorOli <63414715+MajorOli@users.noreply.github.com>
2023-09-09 10:43:47 +0200

    https://github.com/Sebbo94BY/teamspeak-dynamic-banner/pull/139#discussion_r1320421794

--- laravel/resources/views/layouts/footer-main.blade.php
+++ laravel/resources/views/layouts/footer-main.blade.php
@@ -1,7 +1,19 @@
 <div class="container mt-auto">
     <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <div class="col-md-6 d-flex align-items-center">
-            <span class="text-muted">&copy; 2023 by <a href="https://github.com/Sebbo94BY" target="_blank">Sebbo94BY</a> | Make code with love ❤️❤️❤️</span>
+        <div class="col-md-12 d-flex align-items-center">
+            <span class="text-muted">
+                &copy; {{\Illuminate\Support\Carbon::now()->format('Y')}} by
+                <a href="https://github.com/Sebbo94BY" target="_blank">Sebbo94BY</a> and
+                <a href="https://github.com/Sebbo94BY/teamspeak-dynamic-banner/graphs/contributors" target="_blank">Contributors</a> |
+                Powered by
+                <i class="fa-brands fa-html5"></i>
+                <i class="fa-brands fa-php"></i>
+                <i class="fa-brands fa-laravel"></i>
+                <i class="fa-brands fa-bootstrap"></i>
+                <i class="fa-solid fa-font-awesome"></i>
+                <i class="fa-brands fa-github"></i> |
+                <a href="https://github.com/Sebbo94BY/teamspeak-dynamic-banner/issues/new/choose" target="_blank">Open feature request or report issue</a>
+            </span>
         </div>
     </footer>
 </div>
