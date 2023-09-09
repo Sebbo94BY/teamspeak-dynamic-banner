c41c61067e061f6ec2e098d8c554a3eb9c196995
MajorOli <63414715+MajorOli@users.noreply.github.com>
2023-09-09 11:24:52 +0200

    https://github.com/Sebbo94BY/teamspeak-dynamic-banner/pull/139#discussion_r1320356272

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,7 +1,6 @@
 import * as bootstrap from 'bootstrap';
 window.bootstrap = bootstrap;
 
-import 'datatables.net-bs5/css/dataTables.bootstrap5.css';
 import 'datatables.net-bs5/js/dataTables.bootstrap5';
 
 import jQuery from 'jquery';
