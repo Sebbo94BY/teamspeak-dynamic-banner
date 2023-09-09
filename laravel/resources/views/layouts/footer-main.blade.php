6ecc28b8ae75d39cc0721256a6dd04654575f57b
MajorOli <63414715+MajorOli@users.noreply.github.com>
2023-09-09 14:29:10 +0200

    footer fix

--- laravel/resources/views/layouts/footer-main.blade.php
+++ laravel/resources/views/layouts/footer-main.blade.php
@@ -1,10 +1,10 @@
 <div class="container mt-auto">
     <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <div class="col-md-12 d-flex align-items-center">
+        <div class="col-md-12 d-flex align-items-center justify-content-between">
             <span class="text-muted">
                 &copy; {{\Illuminate\Support\Carbon::now()->format('Y')}} by
                 <a href="https://github.com/Sebbo94BY" target="_blank">Sebbo94BY</a> and
-                <a href="https://github.com/Sebbo94BY/teamspeak-dynamic-banner/graphs/contributors" target="_blank">Contributors</a> |
+                <a href="https://github.com/Sebbo94BY/teamspeak-dynamic-banner/graphs/contributors" target="_blank">Contributors</a>
                 Powered by
                 <i class="fa-brands fa-html5"></i>
                 <i class="fa-brands fa-php"></i>
@@ -12,6 +12,8 @@
                 <i class="fa-brands fa-bootstrap"></i>
                 <i class="fa-solid fa-font-awesome"></i>
                 <i class="fa-brands fa-github"></i> |
+            </span>
+            <span>
                 <a href="https://github.com/Sebbo94BY/teamspeak-dynamic-banner/issues/new/choose" target="_blank">Open feature request or report issue</a>
             </span>
         </div>
