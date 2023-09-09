a55715d108d962f2577159634c0df602993cffde
MajorOli <63414715+MajorOli@users.noreply.github.com>
2023-09-09 18:11:10 +0200

    update php unit test

--- laravel/tests/Feature/InstanceTest.php
+++ laravel/tests/Feature/InstanceTest.php
@@ -49,17 +49,6 @@ class InstanceTest extends TestCase
         $response->assertViewIs('instances');
     }
 
-//    /**
-//     * Test, that the user can access the "add instance" page, when he is authenticated.
-//     */
-//    public function test_add_instance_page_gets_displayed_when_authenticated(): void
-//    {
-//        //todo obsolete
-//        $response = $this->actingAs($this->user)->get(route('instance.add'));
-//        $response->assertStatus(200);
-//        $response->assertViewIs('instance.add');
-//    }
-
     /**
      * Test that adding a new instance requires to match the request rules.
      */
@@ -74,47 +63,6 @@ class InstanceTest extends TestCase
         ]);
         $response->assertSessionHasErrors(['host']);
     }
-
-    /**
-     * Test that adding a new instance returns an error when no connection to the instance can be established.
-     *
-     * Disabled because the assertions are failing with this error:
-     *    fwrite(): Argument #1 ($stream) must be of type resource, bool given
-     */
-    // public function test_adding_a_new_instance_returns_an_error_when_no_connection_to_the_instance_can_be_established(): void
-    // {
-    //     $response = $this->actingAs($this->user)->post(route('instance.save'), [
-    //         'host' => fake()->domainName(),
-    //         'voice_port' => fake()->numberBetween(1024, 65535),
-    //         'serverquery_port' => fake()->numberBetween(1024, 65535),
-    //         'serverquery_username' => fake()->userName(),
-    //         'serverquery_password' => fake()->password(),
-    //         'client_nickname' => fake()->name(),
-    //     ]);
-    //     $response->assertRedirectToRoute('instance.add');
-    //     $response->assertSessionHasInput('host');
-    //     $response->assertSessionHasInput('voice_port');
-    //     $response->assertSessionHasInput('serverquery_port');
-    //     $response->assertSessionHasInput('serverquery_password');
-    //     $response->assertSessionHasInput('client_nickname');
-    //     $response->assertSessionHas('error');
-    // }
-
-    /**
-     * Test, that the user can access the "edit instance" page, when the requested instance ID for the edit page exists.
-     *
-     * Disabled because the assertions are failing with this error:
-     *    fwrite(): Argument #1 ($stream) must be of type resource, bool given
-     */
-    // public function test_edit_instance_page_gets_displayed_when_instance_id_exists(): void
-    // {
-    //     $instance = Instance::factory()->create();
-    //
-    //     $response = $this->actingAs($this->user)->get(route('instance.edit', ['instance_id' => $instance->id]));
-    //     $response->assertViewIs('instance.edit');
-    //     $response->assertViewHas('instance');
-    //     $response->assertViewHas('channel_list');
-    // }
 
     /**
      * Test that updating an existing instance requires to match the request rules.
@@ -147,7 +95,7 @@ class InstanceTest extends TestCase
      */
     public function test_starting_an_instance_is_possible(): void
     {
-        $response = $this->actingAs($this->user)->post(route('instance.start', ['instance_id' => $this->instance->id]));
+        $response = $this->actingAs($this->user)->get(route('instance.start', ['instance_id' => $this->instance->id]));
         $response->assertRedirectToRoute('instances');
         $response->assertSessionHas('success');
     }
@@ -159,7 +107,7 @@ class InstanceTest extends TestCase
     {
         InstanceProcess::factory()->for($this->instance)->create();
 
-        $response = $this->actingAs($this->user)->post(route('instance.stop', ['instance_id' => $this->instance->id]));
+        $response = $this->actingAs($this->user)->get(route('instance.stop', ['instance_id' => $this->instance->id]));
         $response->assertRedirectToRoute('instances');
         $response->assertSessionHas('success');
     }
@@ -171,7 +119,7 @@ class InstanceTest extends TestCase
     {
         InstanceProcess::factory()->for($this->instance)->create();
 
-        $response = $this->actingAs($this->user)->post(route('instance.restart', ['instance_id' => $this->instance->id]));
+        $response = $this->actingAs($this->user)->get(route('instance.restart', ['instance_id' => $this->instance->id]));
         $response->assertRedirectToRoute('instances');
         $response->assertSessionHas('success');
     }
