03e83dfef1b06c368c6ca1b5dd1e3e5f2179f649
MajorOli <63414715+MajorOli@users.noreply.github.com>
2023-09-10 10:39:03 +0200

    https://github.com/Sebbo94BY/teamspeak-dynamic-banner/pull/139#discussion_r1320660414

--- laravel/app/Http/Controllers/Auth/LoginController.php
+++ laravel/app/Http/Controllers/Auth/LoginController.php
@@ -4,14 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
-use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Contracts\View\Factory;
-use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Routing\Redirector;
-use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -33,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected string $redirectTo = RouteServiceProvider::HOME;
 
     /**
      * Create a new controller instance.
