125ae2024c60a2a2722a5c59e2f4207e6b95480a
MajorOli <63414715+MajorOli@users.noreply.github.com>
2023-09-10 10:36:30 +0200

    Update laravel/app/Http/Controllers/Administration/PhpInfoController.php
    Co-authored-by: Sebbo94BY <Sebastian-Kraetzig@gmx.de>

--- laravel/app/Http/Controllers/Administration/PhpInfoController.php
+++ laravel/app/Http/Controllers/Administration/PhpInfoController.php
@@ -14,7 +14,7 @@ class PhpInfoController extends Controller
     {
         ob_start();
         phpinfo();
-        $phpInfo = ob_get_contents();
+        $phpinfo_full_html = ob_get_contents();
         ob_end_clean();
         $phpInfo = preg_replace('%^.*<body>(.*)</body>.*$%ms', '$1', $phpInfo);
 
