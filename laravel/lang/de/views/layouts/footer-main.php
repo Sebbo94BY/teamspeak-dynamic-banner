e26cbc6cd6d131960bd753b587c17dcccad95763
Sebbo94BY <sebastian-kraetzig@gmx.de>
2023-09-27 20:33:17 +0200

    Translate Footer

---
+++ laravel/lang/de/views/layouts/footer-main.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    /**
+     * Main Footer
+     */
+    'contributors' => 'Mitwirkende',
+    'powered_by' => 'Angetrieben von',
+    'feature_request_report_issue_link' => 'Feature-Anfrage öffnen oder Problem melden',
+
+];
