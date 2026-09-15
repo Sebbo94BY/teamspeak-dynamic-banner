ce9b1a8a2058aa117f37026517ab8d7be0019ab8
skraetzig <sebastian.kraetzig@tradebyte.com>
2026-09-16 00:46:16 +0200

    Migrate application configuration for Laravel 13

--- laravel/config/sanctum.php
+++ laravel/config/sanctum.php
@@ -62,8 +62,8 @@ return [
     'middleware' => [
         'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
         'encrypt_cookies' => Illuminate\Cookie\Middleware\EncryptCookies::class,
-        'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
-        'verify_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
+        'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\PreventRequestForgery::class,
+        'verify_csrf_token' => Illuminate\Foundation\Http\Middleware\PreventRequestForgery::class,
     ],
 
 ];
