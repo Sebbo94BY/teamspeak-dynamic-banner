fd5d9f3606c3bfeca04ffbfe3a044ca6e134c1ee
skraetzig <sebastian.kraetzig@tradebyte.com>
2026-09-23 20:06:20 +0200

    Put available variables into a filterable datatable

--- laravel/lang/en/views/modals/modal-variables.php
+++ laravel/lang/en/views/modals/modal-variables.php
@@ -12,6 +12,15 @@ return [
      */
     'table_variable_name' => 'Name',
     'table_variable_value' => 'Value',
+    'datatable_all' => 'All',
+    'datatable_empty' => 'No variables available',
+    'datatable_info' => '_START_ to _END_ of _TOTAL_ variables',
+    'datatable_info_empty' => 'No variables available',
+    'datatable_length_menu' => '_MENU_ variables per page',
+    'datatable_search' => 'Filter variables:',
+    'datatable_zero_records' => 'No matching variables found',
+    'datatable_previous' => 'Previous',
+    'datatable_next' => 'Next',
 
     /**
      * Buttons
